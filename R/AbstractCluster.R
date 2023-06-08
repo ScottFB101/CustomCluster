08b0fbe6117acb9d1c9e4f39d8fd429d3266e53d
ScottFB101 <113871715+ScottFB101@users.noreply.github.com>
2023-06-07 21:06:49 -0700

    Created package setup

---
+++ R/AbstractCluster.R
@@ -0,0 +1,15 @@
+#' Implements
+#'
+#' @param dat A data set
+#' @param k The number of desired clusters
+#'
+#' @return Returns cluster assignments
+#'
+#' @import dplyr
+#' @import stats
+#'
+#' @export
+
+AbstractCluster <- function() {
+
+}
