72275cca946d245143b6e6f1dd486072e9103133
ScottFB101 <113871715+ScottFB101@users.noreply.github.com>
2023-06-08 16:10:57 -0700

    Having to figure out how to subset data each iteration

--- R/AbstractCluster.R
+++ R/AbstractCluster.R
@@ -20,14 +20,7 @@ AbstractCluster <- function(dat, distance = 1, neighbors = 5) {
   #Number of rows/observations
   num_obs <- nrow(dat)
 
-  #Randomly select 1 observations
-  random_obs <- sample(num_obs, size = 1)
-
-  #Create starting center for reference
-  starting_center <- slice(dat, random_obs)
-
   #Vectors of clusters
-  clusters <- c(1)
   last_clusters <- c(0)
   border_points <- c(0)
   dist <- c(0)
@@ -41,34 +34,53 @@ AbstractCluster <- function(dat, distance = 1, neighbors = 5) {
   #4. For loop to check if the points just added in that cluster have enough "neighbor" points within that distance
   #5. If so, add them to cluster
 
-  #Loop to calculate Euclidean distance of each point from the randomly selected centers
-  for(i in 1:num_obs) {
+  #Original_data[sub_dat$row_index, "cluster"
+  #Keep sub-setting data throughout the loop
 
-    #Attaching observation to data frame of the randomly chosen center
-    center_and_point <- dat[i, ] %>% rbind(starting_center)
+  repeat {
 
-    #Measuring Euclidean distance between observation and randomly chosen center
-    dist[i] <- dist(center_and_point, method = "euclidean", upper = FALSE)
+    #Need to subset dat to remove points that have been selected
+    #Create a way to remember the row index, so we can reinsert cluster assignments to the correct observation
+
+
+    #Randomly select 1 observations
+    random_obs <- sample(num_obs, size = 1)
+
+    #Create starting center for reference
+    starting_center <- slice(dat, random_obs)
+
+    #Loop to calculate Euclidean distance of each point from the randomly selected centers
+    for(i in 1:num_obs) {
+
+      #Attaching observation to data frame of the randomly chosen center
+      center_and_point <- dat[i, ] %>% rbind(starting_center)
+
+      #Measuring Euclidean distance between observation and randomly chosen center
+      dist[i] <- dist(center_and_point, method = "euclidean", upper = FALSE)
+
+    }
+
+    #Test if distance between points and center_point is within distance declared by user
+    core_points <- (dist < distance)
+
+    #If TRUE, then assign those observation's row value to the cluster
+    cluster_assignment[core_points == TRUE] <- cluster
+
+    #Add cluster assignments to original data set
+    dat <- dat %>%
+      cbind(cluster_assignment)
+
+    #Add one to cluster to indicate next iteration of clustering
+    cluster <- cluster + 1
+
+    browser()
+    #If all of the cluster assignment has been
+    if(all(is.na(dat$cluster_assignment)) == TRUE) {
+      break
+    }
 
   }
-
-  #Test if distance between points and center_point is within distance declared by user
-  core_points <- (dist < distance)
-
-  #If TRUE, then assign those observation's row value to the cluster #
-  cluster_assignment[core_points == TRUE] <- cluster
-
-  #Add cluster assignments to original data set
-  dat <- dat %>%
-    cbind(cluster_assignment)
-
-  #Add one to cluster to indicate next iteration of clustering
-  cluster <- cluster + 1
 
   return(dat)
 
 }
-
-#Original_data[sub_dat$row_index, "cluster"]
-#Keep subsetting data throughout the loop
-
